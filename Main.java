e0278e8e21511f19b158b4becd114acf61e3d537
balez4464 <balez4464@wrdsb.ca>
2020-10-06 14:28:23 +0000

    Made a program to give the user their multiplication table

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This proram shows a multiplication table for the users given number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user to input number
+    System.out.println("Please enter an integer to create a multiplication table for");
+    //creates intergers for the usersnumber and the qoucient
+    int userNumber = input.nextInt();
+    int qoucient;
+
+    //creates for loop for integer
+    for(int multicator = 1; multicator <= 12; multicator++){
+      //multiplys to get quoicent
+      qoucient = userNumber * multicator;
+      //prints out the given equation
+      System.out.println(multicator + " x " + userNumber + " = " + qoucient);
+    }
     
   }
 }
